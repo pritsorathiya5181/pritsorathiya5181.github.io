bfee3ea7c03ca38430838f0b66bd0df532c74123
prit <prit.sorathiya@gmail.com>
2020-08-15 21:14:58 +0530

    Updates

---
+++ precache-manifest.a4b4ae4b392b4556990d375446699a12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e1e3dc9936ca87cf80f7f4772877c61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "688f5530953494e86838",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "60f00aac250a04bda7b5",
+    "url": "/static/js/2.17e33530.chunk.js"
+  },
+  {
+    "revision": "688f5530953494e86838",
+    "url": "/static/js/main.23d97fa5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1ac009320d663a6434ebbfc35714daee",
+    "url": "/static/media/1.1ac00932.jpeg"
+  }
+]);
